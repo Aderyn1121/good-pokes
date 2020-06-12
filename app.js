433b2415042bdbc1c8269faa0bf1a5e1eae8cdbf
Addy <zaristus@gmail.com>
2020-06-12 15:56:35 -0700

    heroku ready

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(cors({ origin }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/reviews', reviewRouter);
